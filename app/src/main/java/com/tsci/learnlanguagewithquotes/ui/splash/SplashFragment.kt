9064fe8bc646c20d7b249b68cbd18dc3b39e2cb0
buraaktascii <buraaktascii@outlook.com>
2022-07-31 14:05:16 +0300

    add | splash screen feature

--- app/src/main/java/com/tsci/learnlanguagewithquotes/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/tsci/learnlanguagewithquotes/ui/splash/SplashFragment.kt
@@ -1,9 +1,13 @@
 package com.tsci.learnlanguagewithquotes.ui.splash
 
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.tsci.learnlanguagewithquotes.R
 import com.tsci.learnlanguagewithquotes.core.BaseFragment
 import com.tsci.learnlanguagewithquotes.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>() {
@@ -13,7 +17,14 @@ class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>() {
     override fun viewModelClass(): Class<SplashViewModel> = SplashViewModel::class.java
 
     override fun initView() {
-
+        lifecycleScope.launch{
+            delay(DELAY)
+            val action = SplashFragmentDirections.toOnBoardingFragment()
+            findNavController().navigate(action)
+        }
     }
 
+    companion object{
+        private const val DELAY = 2000L
+    }
 }
