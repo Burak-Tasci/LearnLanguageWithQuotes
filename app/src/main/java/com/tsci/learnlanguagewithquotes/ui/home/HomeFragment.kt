59bcc4912ab992e894709753cc5104a4380ca344
buraaktascii <buraaktascii@outlook.com>
2022-08-07 21:10:54 +0300

    add | quote attributes to views

--- app/src/main/java/com/tsci/learnlanguagewithquotes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tsci/learnlanguagewithquotes/ui/home/HomeFragment.kt
@@ -1,18 +1,30 @@
 package com.tsci.learnlanguagewithquotes.ui.home
 
+import androidx.lifecycle.lifecycleScope
 import com.tsci.learnlanguagewithquotes.R
+import com.tsci.learnlanguagewithquotes.common.extension.setChips
 import com.tsci.learnlanguagewithquotes.core.BaseFragment
 import com.tsci.learnlanguagewithquotes.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel>() {
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
 
     override fun layoutRes(): Int = R.layout.fragment_home
 
     override fun viewModelClass(): Class<HomeViewModel> = HomeViewModel::class.java
 
-    override fun initView() {
-
+    override fun initView(): Unit = with(binding) {
+        viewModel.getQuote()
+        lifecycleScope.launch{
+            viewModel.quote.collect{
+                tvQuote.text = it.content
+                tvOwner.text = it.owner.name
+                cpTags.setChips(it.tags){}
+            }
+        }
     }
+
 }
