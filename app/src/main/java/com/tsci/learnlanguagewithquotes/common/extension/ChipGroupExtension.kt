0e852a828dd6acc197f590b868a2dc3ef74f134b
buraaktascii <buraaktascii@outlook.com>
2022-08-07 16:32:00 +0300

    update | chip group layout constraints & add | long click listener for chip item

--- app/src/main/java/com/tsci/learnlanguagewithquotes/common/extension/ChipGroupExtension.kt
+++ app/src/main/java/com/tsci/learnlanguagewithquotes/common/extension/ChipGroupExtension.kt
@@ -6,11 +6,12 @@ import com.google.android.material.chip.ChipGroup
 import com.tsci.learnlanguagewithquotes.R
 
 
-fun ChipGroup.setChips(list: List<String>){
+fun ChipGroup.setChips(list: List<String>, onLongClick: (Int) ->Unit){
     for (element in list){
         val chip = LayoutInflater.from(context)
             .inflate(R.layout.item_chip, this,false) as Chip
         chip.text = element
+        chip.setOnLongClickListener{onLongClick(list.indexOf(element));true }
         this.addView(chip)
     }
 }
