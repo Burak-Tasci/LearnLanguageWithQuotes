44f3cf726271a96e51385645f2c143beb2fd1aab
buraaktascii <buraaktascii@outlook.com>
2022-08-08 00:47:56 +0300

    update | onboarding dropdown menus & add | preferences manager functions

--- app/src/main/java/com/tsci/learnlanguagewithquotes/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/com/tsci/learnlanguagewithquotes/ui/onboarding/OnBoardingFragment.kt
@@ -1,22 +1,34 @@
 package com.tsci.learnlanguagewithquotes.ui.onboarding
 
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
 import com.tsci.learnlanguagewithquotes.R
 import com.tsci.learnlanguagewithquotes.core.BaseFragment
 import com.tsci.learnlanguagewithquotes.databinding.FragmentOnBoardingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class OnBoardingFragment : BaseFragment<FragmentOnBoardingBinding, OnBoardingViewModel>() {
 
+    override fun viewModelClass(): Class<OnBoardingViewModel> = OnBoardingViewModel::class.java
     override fun layoutRes(): Int = R.layout.fragment_on_boarding
 
-    override fun initView() {
+    override fun initView() = with(binding) {
 
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.item_language,
+            resources.getStringArray(R.array.languages)
+        )
+        actvNative.setAdapter(adapter)
+        actvTarget.setAdapter(adapter)
+
+//        viewModel.setOnboardingPreference(false)
     }
-    override fun viewModelClass(): Class<OnBoardingViewModel> = OnBoardingViewModel::class.java
 
 
-    override fun initListeners() = with(binding){
+    override fun initListeners() = with(binding) {
         btnSubmit.setOnClickListener {
             findNavController().navigate(
                 OnBoardingFragmentDirections.toHomeFragment()
--- app/src/main/java/com/tsci/learnlanguagewithquotes/ui/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/tsci/learnlanguagewithquotes/ui/onboarding/OnBoardingViewModel.kt
@@ -1,11 +1,49 @@
 package com.tsci.learnlanguagewithquotes.ui.onboarding
 
+import androidx.lifecycle.viewModelScope
 import com.tsci.learnlanguagewithquotes.core.BaseViewModel
+import com.tsci.learnlanguagewithquotes.data.local.preferences.PreferencesManager
+import com.tsci.learnlanguagewithquotes.domain.use_case.onboarding.OnBoardingValidationParam
+import com.tsci.learnlanguagewithquotes.domain.use_case.onboarding.OnBoardingValidationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor(
+    private val preferencesManager: PreferencesManager,
+    private val onBoardingValidationUseCase: OnBoardingValidationUseCase
+) : BaseViewModel() {
 
-) :BaseViewModel(){
+    private val _nativeLanguage: MutableStateFlow<String> = MutableStateFlow("")
+    val nativeLanguage = _nativeLanguage.asStateFlow()
+
+    private val _targetLanguage: MutableStateFlow<String> = MutableStateFlow("")
+    val targetLanguage = _targetLanguage.asStateFlow()
+
+    private val _isButtonEnabled = MutableStateFlow(false)
+    val isButtonEnabled = _isButtonEnabled.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            combine(nativeLanguage, targetLanguage) { native, target ->
+                return@combine onBoardingValidationUseCase.invoke(
+                    OnBoardingValidationParam(
+                        native,
+                        target
+                    )
+                )
+            }.collect {
+                _isButtonEnabled.value = it
+            }
+        }
+    }
+
+
+    fun setOnboardingPreference(hasOnBoarding: Boolean) {
+        preferencesManager.setIsFirstOpening(hasOnBoarding)
+    }
 }
