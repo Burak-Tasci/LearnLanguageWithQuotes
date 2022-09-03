5391d1fdeac29e07c753e08db130578bdb09eb0f
buraaktascii <buraaktascii@outlook.com>
2022-09-03 21:57:23 +0300

    update | application as fullscreen

--- app/src/main/java/com/tsci/learnlanguagewithquotes/ui/MainActivity.kt
+++ app/src/main/java/com/tsci/learnlanguagewithquotes/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.tsci.learnlanguagewithquotes.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.tsci.learnlanguagewithquotes.R
 import com.tsci.learnlanguagewithquotes.databinding.ActivityMainBinding
@@ -15,11 +19,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setStatusBar()
         setContentView(binding.root)
+        setSystemWindows()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
     }
 
 
+    private fun setStatusBar() {
+        window.statusBarColor = ContextCompat.getColor(this, R.color.window_color)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = 0x00000000  // transparent
+    }
+    private fun setSystemWindows() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+    }
 }
