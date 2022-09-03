46c7cfa16dcbe772b684f7a5567f167774ec9b61
buraaktascii <buraaktascii@outlook.com>
2022-09-03 23:25:29 +0300

    config | timber library

--- app/src/main/java/com/tsci/learnlanguagewithquotes/QuotesApp.kt
+++ app/src/main/java/com/tsci/learnlanguagewithquotes/QuotesApp.kt
@@ -2,6 +2,17 @@ package com.tsci.learnlanguagewithquotes
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
+
 
 @HiltAndroidApp
-class QuotesApp: Application()
+class QuotesApp: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            plant(Timber.DebugTree())
+        }
+    }
+}
